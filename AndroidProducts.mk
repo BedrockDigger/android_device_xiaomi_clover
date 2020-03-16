02fd6390ac3dc5e254d2b0ded30278d086dc77b5
Estel <callmebedrockdigger@gmail.com>
2020-03-16 23:03:13 +0800

    Evolution X: bring up

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -23,9 +23,9 @@
 
 # inherit MI PAD 4 lineage-OS device config
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/carbon_clover.mk
+    $(LOCAL_DIR)/aosp_clover.mk
 
 COMMON_LUNCH_CHOICES := \
-		carbon_clover-eng \
-		carbon_clover-userdebug \
-		carbon_clover-user
+		aosp_clover-eng \
+		aosp_clover-userdebug \
+		aosp_clover-user
--- aosp_clover.mk
+++ aosp_clover.mk
@@ -24,23 +24,23 @@
 # Inherit device configuration
 $(call inherit-product, device/xiaomi/clover/device.mk)
 
-# Inherit Carbon GSM telephony parts
-$(call inherit-product, vendor/carbon/config/gsm.mk)
+# Inherit Evo-X product configuration
+TARGET_BOOT_ANIMATION_RES := 1200x1920
+TARGET_GAPPS_ARCH := arm64
 
-# Inherit Carbon product configuration
-$(call inherit-product, vendor/carbon/config/common.mk)
+# Inherit EvoX GSM telephony parts
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+
+# Inherit EvoX product configuration
+$(call inherit-product, vendor/aosp/config/common.mk)
 
 # Define first api level
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
-# CarbonRom Maintainer
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.carbon.maintainer="Rcstar6696"
-
 # Device identifier
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := carbon_clover
+PRODUCT_NAME := aosp_clover
 PRODUCT_CHARACTERISTICS := tablet
 PRODUCT_DEVICE := clover
 PRODUCT_MODEL := MI PAD 4
